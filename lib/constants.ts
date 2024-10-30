f4819b396c7a2778c3ef5cfd7e775f4dd926c1f1
harshraj8055 <harsh.raj21b@iiitg.ac.in>
2024-10-30 23:27:23 +0530

    final

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,6 +3,6 @@ const GITHUB_LINK = "https://github.com/programmer-Harsh Raj";
 const INSTAGRAM_LINK = "https://www.instagram.com/Harsh Raj262830";
 const LINKEDIN_LINK = "https://www.linkedin.com/in/harsh-raj-522935134/";
 const RESUME_LINK =
-  "https://drive.google.com/file/d/164x4WAOuwsFjBYsFQ_x4WhAZfh1p4OeN/view?usp=sharing";
+  "https://drive.google.com/file/d/19VsQVpz1NNkK_Vn9NHFid3NbkLTHi6qt/view?usp=sharing";
 
 export { GMAIL_LINK, GITHUB_LINK, INSTAGRAM_LINK, LINKEDIN_LINK, RESUME_LINK };
